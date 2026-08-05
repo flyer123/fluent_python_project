92af3d00fd70cb1c7b16fd53aa72d473fad74fde
Flyer123 <shirinskiy.yuriy@gmail.com>
2026-08-05 21:38:37 +0300

    clean events finished and fixed

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from ingestion.load_events import load_events
-from processing.events_per_country import events_per_country
-from processing.purchases_per_device import purchases_per_device
-from processing.revenue_per_country import revenue_per_country
-from processing.unique_users import unique_users
-from processing.sort_events_by_revenue import sort_events_by_revenue
-from processing.group_by_event_type import group_by_event_type
+from reports.events_per_country import events_per_country
+from reports.purchases_per_device import purchases_per_device
+from reports.revenue_per_country import revenue_per_country
+from reports.unique_users import unique_users
+from reports.sort_events_by_revenue import sort_events_by_revenue
+from reports.group_by_event_type import group_by_event_type
+from processing.clean_events import clean_events
 
 events = load_events("../data/events.csv")
 
@@ -12,9 +13,13 @@ print(len(events))
 
 print(events[:3])
 
-print(events_per_country(events))
-print(purchases_per_device(events))
-print(revenue_per_country(events))
-print(unique_users(events))
-print(sort_events_by_revenue(events))
-print(group_by_event_type(events))
+cleaned_events = clean_events(events)
+print(len(cleaned_events))
+
+print(cleaned_events[:3])
+print(events_per_country(cleaned_events))
+print(purchases_per_device(cleaned_events))
+print(revenue_per_country(cleaned_events))
+print(unique_users(cleaned_events))
+print(sort_events_by_revenue(cleaned_events))
+print(group_by_event_type(cleaned_events)) 
--- src/processing/clean_events.py
+++ src/processing/clean_events.py
@@ -1,40 +1,44 @@
 from models.Event import Event
 import re
 
-def clean_field(func, field):
-    return func(field)
-
-def check_convert(type_to_check, field):
+def check_convert(field, type_to_check):
     try:
         type_to_check(field)
     except:
         return False
     return True
 
-def check_timestamp(timestamp):
-    if re.fullmatch(pattern, test_string):
+def check_timestamp(pattern, timestamp):
+    if re.fullmatch(pattern, timestamp):
         return True
     else:
         return False
 
+
 pattern = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$'
 def clean_events(events):
-    return [
-        if check_timestamp(event.timestamp) and check_convert(event_id, int) and check_convert(revenue, float)
-            Event(
-                event_id=int(event.event_id)
-                user_id=clean_field(clean_field(event.user_id, lower), trim)
-                source=clean_field(clean_field(event.source, lower), trim)
-                event_type=clean_field(clean_field(event.event_type, lower), trim)
-                device=clean_field(clean_field(event.device, lower), trim)
-                country=clean_field(clean_field(event.country, upper), trim)
-                revenue=float(event.revenue)
+    event_ids=[]
+    cleaned_events = []
+    for event in events:
+        if check_timestamp(pattern, event.timestamp) \
+        and check_convert(event.event_id, int) \
+        and check_convert(event.revenue, float) \
+        and event.event_id not in event_ids:
+            event = Event(
+                event_id=int(event.event_id),
+                user_id=event.user_id.lower().strip(),
+                source=event.source.lower().strip(),
+                event_type=event.event_type.lower().strip(),
+                device=event.device.lower().strip(),
+                country=event.country.upper().strip(),
+                revenue=float(event.revenue),
                 timestamp=event.timestamp
-        ) 
+                ) 
+            cleaned_events.append(event)
+            event_ids.append(event.event_id)
         else:
             pass
-        for event in events
-    ]
+    return cleaned_events
 
 
-# implement checking for duplicate
+
